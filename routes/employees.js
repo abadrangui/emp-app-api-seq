99d9dafa51f663e6aeb78325d6495718d31f7554
Badrangui ARIUNBOLD <abadrangui@gmail.com>
2020-05-05 18:51:54 +0800

    employee edit remove done

--- routes/employees.js
+++ routes/employees.js
@@ -23,16 +23,52 @@ Route.get('/findall/:id', (req, res) => {
 
 Route.post('/add', (req, res) => {
   Model.create(req.body)
-  .then(() => {
-    res.sendStatus(204)
+    .then(() => {
+      res.sendStatus(204)
+    })
+    .catch(err => {
+      console.log(err);
+      res.json({ er: err });
+    })
+})
+
+Route.put('/edit', (req, res) => {
+  const { id } = req.body
+  Model.findOne({ where: { id } })
+    .then(emp => {
+      if (emp) {
+        emp.update(req.body)
+          .then(() => { res.sendStatus(204) })
+          .catch(err => {
+            console.log(err);
+            res.json({ er: err });
+          })
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.json({ er: err });
+    })
+})
+
+Route.delete('/edit', (req, res) => {
+  const {id} = req.body;
+
+  Model.findOne({where: {id}})
+  .then(emp => {
+    if(emp) {
+      emp.destroy()
+      .then(() => {res.sendStatus(204)})
+      .catch(err => {
+        console.log(err);
+        res.json({ er: err });
+      })
+    }
   })
   .catch(err => {
     console.log(err);
     res.json({ er: err });
   })
 })
-
-
-
 
 module.exports = Route;
