37591299fe3804f134ce7b8ae590c1ce955f6203
Badrangui ARIUNBOLD <abadrangui@gmail.com>
2020-05-09 16:29:22 +0800

    some Console

--- models/index.js
+++ models/index.js
@@ -8,6 +8,8 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
+console.log("env", env);
+console.log("config", config);
 
 let sequelize;
 if (config.use_env_variable) {
